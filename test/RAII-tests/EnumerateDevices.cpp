08edd6269204f7579e5162cbbc685eaabdcff94a
ImGili <18017158729@163.com>
2023-02-14 13:44:58 +0800

    ✨ feat: raii physicaldevice

--- test/RAII-tests/EnumerateDevices.cpp
+++ test/RAII-tests/EnumerateDevices.cpp
@@ -2,44 +2,20 @@
 #include <iostream>
 namespace RAII
 {
-    static std::string AppName = "01_InitInstanceRAII";
+    static std::string AppName = "EnumerateDevices";
     static std::string EngineName = "Vulkan.hpp";
 
     int EnumerateDevices()
     {
-        /* VULKAN_HPP_KEY_START */
-        try
-        {
-            // the very beginning: instantiate a context
-            vk::raii::Context context;
+        vk::raii::Context context;
 
-            // initialize the vk::ApplicationInfo structure
-            vk::ApplicationInfo applicationInfo(AppName.c_str(), 1, EngineName.c_str(), 1, VK_API_VERSION_1_1);
+        vk::raii::Instance instance = vk::raii::su::makeInstance(context, AppName, EngineName);
 
-            // initialize the vk::InstanceCreateInfo
-            vk::InstanceCreateInfo instanceCreateInfo({}, &applicationInfo);
+#if !defined(NDEBUG)
+        vk::raii::DebugUtilsMessengerEXT debugUilesMessager(instance, vk::su::makeDebugUtilsMessengerCreateInfoEXT());
+#endif // !defined(NDEBUG)
 
-            // create an Instance
-            vk::raii::Instance instance(context, instanceCreateInfo);
-        }
-        catch (vk::SystemError& err)
-        {
-            std::cout << "vk::SystemError: " << err.what() << std::endl;
-            exit(-1);
-        }
-        catch (std::exception& err)
-        {
-            std::cout << "std::exception: " << err.what() << std::endl;
-            exit(-1);
-        }
-        catch (...)
-        {
-            std::cout << "unknown error\n";
-            exit(-1);
-        }
-
-        /* VULKAN_HPP_KEY_END */
-
+        vk::raii::PhysicalDevices pyhsicalDevices(instance);
         return 0;
     }
 }
